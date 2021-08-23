0e315d99f4c1623b4331e8f2b3051b269f7b62f4
Daniel Zamudio <daniel.zamudio@ymail.com>
2021-08-23 12:59:09 -0500

    checkpoint

---
+++ vue_crash_todolist/src/components/layout/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <Header class="header">
+        <h1>TodoList</h1>
+    </Header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+.header {
+    background: #333;
+    color: #fff;
+    text-align: center;
+    padding: 10px;
+}
+
+.header a {
+    color: #fff;
+    padding-right: 5px;
+}
+</style>
